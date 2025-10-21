bf669e37f7aefb10d17ddd636641390f28746627
ManasehShamim <manasehshamim@gmail.com>
2025-10-21 22:48:07 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="custom_corpus_qna",
+    version="0.1.0",
+    author="Manaseh Shamim",
+    author_email="manasehshamim@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
